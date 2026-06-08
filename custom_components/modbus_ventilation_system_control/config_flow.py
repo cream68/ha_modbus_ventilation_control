094b1f646f6d5db3c07cdcd23f316d4f2df1ab20
creamtv <creamtv.68@googlemail.com>
2026-06-08 21:58:55 +0200

    config flow

--- custom_components/modbus_ventilation_system_control/config_flow.py
+++ custom_components/modbus_ventilation_system_control/config_flow.py
@@ -57,7 +57,7 @@ class VentilationConfigFlow(ConfigFlow, domain=DOMAIN):
 
 class VentilationOptionsFlow(OptionsFlow):
     def __init__(self, config_entry: ConfigEntry) -> None:
-        super().__init__(config_entry)
+        super().__init__()
         self._config_entry = config_entry
 
     async def async_step_init(self, user_input: dict[str, Any] | None = None):
